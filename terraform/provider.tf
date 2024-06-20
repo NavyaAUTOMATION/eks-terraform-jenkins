b82bdd64b9e24f19a7b58ae80478e3ed0ff1be8e
NavyaDatla <86724469+NavyaAUTOMATION@users.noreply.github.com>
2024-06-21 00:25:14 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,9 @@
 
 locals {
-  region = "eu-north-1"
-  name   = "eks-demo-cluster"
+  region = "us-east-1"
+  name   = "eks-demo-cluster1"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["eu-north-1a", "eu-north-1b"]
+  azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
@@ -13,5 +13,5 @@ locals {
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
